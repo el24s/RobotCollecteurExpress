fe8265f9e4f23099960524dff823be7a27448787
Ellyn Saint-Firmin <6309487@cegepmv.ca>
2026-09-04 14:37:50 -0400

    Ajouter la porte de sortie

--- Assets/Scripts/PorteSortie.cs
+++ Assets/Scripts/PorteSortie.cs
@@ -4,17 +4,24 @@ public class PorteSortie : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D autre)
     {
-        // TODO : ignorer tout objet qui n'est pas le joueur.
-        // TODO : annoncer la réussite et faire disparaître le joueur.
+        // ignorer tout objet qui n'est pas le joueur.
+        if (!autre.CompareTag("Player"))
+        {
+            return;
+        }
+        
+        // annoncer la réussite et faire disparaître le joueur.
+        Debug.Log("MISSION RÉUSSIE !");
+        Destroy(autre.gameObject);
     }
 
     /*
      * BANQUE DE LIGNES — GROUPE B
      * Replacez les lignes, puis ajoutez les accolades manquantes.
      *
-     * if (!autre.CompareTag("Player"))
-     * Debug.Log("MISSION RÉUSSIE !");
-     * return;
-     * Destroy(autre.gameObject);
+     * 
+     * 
+     * 
+     * 
      */
 }
